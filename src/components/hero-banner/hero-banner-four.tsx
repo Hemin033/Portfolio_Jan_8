5d9db229610e6e2b1681a195ffc2cdd627250b19
Hemin033 <heminshah97@gmail.com>
2025-09-05 20:03:42 -0400

    fix hero

--- src/components/hero-banner/hero-banner-four.tsx
+++ src/components/hero-banner/hero-banner-four.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 
 export default function HeroBannerFour() {
   return (
-    <div className="tp-hero-3-area tp-hero-3-ptb fix">
-      <div className="container">
+    <div className="tp-hero-3-area tp-hero-3-ptb fix d-flex align-items-center justify-content-center min-vh-50">
+      <div className="container ">
         <div className="row">
           <div className="col-xl-12">
             <div className="tp-hero-3-content-box text-center p-relative">
@@ -14,11 +14,11 @@ export default function HeroBannerFour() {
                 <span></span>
               </div>
               <h4 className="tp-hero-3-title hemin-hero-title tp_reveal_anim mb-4">
-                <span className="tp-reveal-line">Hi, I'm Hemin</span>
-                <span className="tp-reveal-line" style={{paddingBottom: "20px", width: "110%", display: "inline-block"}}>Almost award-winning designer</span>
+                <span className="tp-reveal-line mt-40">Hi, I'm Hemin</span>
+                <span className="tp-reveal-line" style={{paddingBottom: "20px", width: "100%", display: "inline-block"}}>Almost award-winning <br /> designer</span>
               </h4>
-              <span className="tp-hero-3-category tp_reveal_anim mb-20" style={{lineHeight: "1.8", width: "100%", display: "block"}}>
-                Graphic Designer & Art Director with a multidisciplinary <br /> approach to solving 
+              <span className="tp-hero-3-category tp_reveal_anim mb-0" style={{lineHeight: "1.8", width: "100%", display: "block"}}>
+                Graphic Designer & Art Director with a multidisciplinary approach to solving 
                 visual problems.
               </span>
               <Link className="tp-btn-black-2 mt-30" href="/contact">
